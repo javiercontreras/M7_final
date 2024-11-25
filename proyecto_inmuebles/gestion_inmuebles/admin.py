9dbde5d9888a0bd6a27646e883eb047aaa44140e
FranciscoVega <pancho.vega.90@gmail.com>
2024-11-25 20:31:15 -0300

    hito1 completo

--- proyecto_inmuebles/gestion_inmuebles/admin.py
+++ proyecto_inmuebles/gestion_inmuebles/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import  Inmueble
+from .models import  Inmueble, Region, Comuna
 # Register your models here.
-admin.site.register(Inmueble)
+
+@admin.register(Inmueble)
+class InmuebleAdmin(admin.ModelAdmin):
+    list_display = ('nombre_inmueble', 'direccion', 'precio')
+    search_fields = ('nombre_inmueble', 'direccion')
+    # list_filter = ('precio', 'region')
+
+admin.site.register(Region)
+admin.site.register(Comuna)
--- proyecto_inmuebles/gestion_inmuebles/models.py
+++ proyecto_inmuebles/gestion_inmuebles/models.py
@@ -18,8 +18,6 @@ class Comuna(models.Model):
       return self.nombre
 
 
-
-   
 class TipoInmueble(models.TextChoices):
    CASA = 'C','Casa'
    DEPARTAMENTO = 'D','Departamento'
@@ -28,6 +26,7 @@ class TipoInmueble(models.TextChoices):
 
 class Inmueble(models.Model):
     nombre = models.ForeignKey(User,on_delete=models.CASCADE)
+    nombre_inmueble = models.CharField(max_length=100, null=False, blank=False, default='')
     descripcion = models.TextField()
     m2_construidos = models.FloatField()
     m2_totales = models.FloatField()
@@ -35,11 +34,11 @@ class Inmueble(models.Model):
     habitaciones = models.IntegerField()
     banos = models.IntegerField()
     direccion = models.CharField(max_length=200)
-    comuna = models.OneToOneField(Comuna, on_delete=models.CASCADE)
+    comuna = models.ForeignKey(Comuna, on_delete=models.CASCADE)
     tipo_inmueble = models.CharField(max_length=1, choices=TipoInmueble.choices, null=False, blank=False, default='C')
-    precio_mensual = models.DecimalField(max_digits=10, decimal_places=2)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
     
     
     def __str__(self):
-     return self.nombre
+     return self.nombre_inmueble
     
